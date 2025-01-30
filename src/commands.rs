c758f2ecdbfd3889df4cafb2e54feca0868466b2
ilBuso <alessandro.busola@gmail.com>
2025-01-30 18:34:36 +0100

    gui

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,7 @@
 use wg_2024::{config::Drone as ConfigDrone, drone::Drone, network::NodeId, packet::Packet};
 
 // From SimCtrl to GUI
+#[derive(Debug, Clone)]
 pub enum GUIEvents {
     PacketSent(NodeId, NodeId, Packet),
     PacketDropped(NodeId, Packet),
@@ -8,8 +9,9 @@ pub enum GUIEvents {
 }
 
 // From GUI to SimCtrl
+#[derive(Debug, Clone)]
 pub enum GUICommands {
-    Spawn(Box<dyn Drone>),
+    Spawn,
     Crash(NodeId),
     RemoveSender(NodeId, NodeId),
     AddSender(NodeId, NodeId),
--- src/gui.rs
+++ src/gui.rs
@@ -1,78 +1,138 @@
+use std::collections::HashMap;
 use crossbeam_channel::{Receiver, Sender, TryRecvError};
 
 use colored::Colorize;
-use eframe::egui;
+use eframe::egui::{self, Color32};
 
-use wg_2024::network::NodeId;
+use wg_2024::{config::Drone as ConfigDrone, network::NodeId};
 
 use crate::{GUICommands, GUIEvents};
 
 pub struct SimCtrlGUI {
     sender: Sender<GUICommands>,
-    receiver: Receiver<GUIEvents>
+    receiver: Receiver<GUIEvents>,
+
+    initialized: bool,
+    drones: Vec<DroneGUI>,
+    edges: HashMap<NodeId, NodeId>
+}
+
+struct DroneGUI {
+    id: NodeId,
+    neighbor: Vec<NodeId>,
+    pdr: f32,
+    x: f32,
+    y: f32,
+    selected: bool,
+    color: egui::Color32
 }
 
 impl SimCtrlGUI {
     pub fn new(sender: Sender<GUICommands>, receiver: Receiver<GUIEvents>) -> Self {
         Self {
             sender,
-            receiver
+            receiver,
+            initialized: false,
+            drones: Vec::new(),
+            edges: HashMap::new()
         }
     }
 
-    /*fn handle_commands(&mut self, drone: NodeId, command: GUICommands) {
+    fn topology(&mut self, topology: Vec<ConfigDrone>) {
+        let mut i = 1;
+
+        for drone in topology {
+            let new_drone = DroneGUI {
+                id: drone.id,
+                neighbor: drone.connected_node_ids,
+                pdr: drone.pdr,
+                x: (i * 100) as f32,
+                y: 100.0,
+                selected: false,
+                color: Color32::BLUE,
+            };
+
+            for drone in new_drone.neighbor.iter() {
+                if !self.edges.contains_key(drone) {
+                    self.edges.insert(new_drone.id.clone(), *drone);
+                }
+            }
+
+            self.drones.push(new_drone);
+
+            i += 1;
+        }
+    }
+
+    fn handle_commands(&mut self, drone: NodeId, command: GUICommands) {
         match command {
-            GUICommands::Spawn(drone) => todo!(),
+            GUICommands::Spawn => todo!(),
             GUICommands::Crash(node_id) => todo!(),
             GUICommands::RemoveSender(drone, neighbor) => todo!(),
             GUICommands::AddSender(drone, neighbor) => todo!(),
             GUICommands::SetPDR(drone, pdr) => todo!(),
         }
-    }*/
+    }
 
     fn handle_events(&mut self, event: GUIEvents) {
         match event {
             GUIEvents::PacketSent(src, dest, packet) => todo!(),
             GUIEvents::PacketDropped(src, packet) => todo!(),
-            GUIEvents::Topology(topology) => todo!()
+            GUIEvents::Topology(topology) => self.topology(topology)
         }
     }
 }
 
 impl eframe::App for SimCtrlGUI {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
-        match self.receiver.try_recv() {
-            Ok(event) => self.handle_events(event),
-            Err(e)=> match e {
-                crossbeam_channel::TryRecvError::Empty => (),
-                crossbeam_channel::TryRecvError::Disconnected => eprintln!(
-                    "[ {} ]: GUICommands receiver channel disconnected: {}",
-                    "Simulation Controller".red(),
-                    e
-                ),
+        if !self.initialized {
+            match self.receiver.try_recv() {
+                Ok(event) => match event.clone() {
+                    GUIEvents::Topology(drones) => self.handle_events(event),
+                    _ => panic!("CAZZZOOOOOOOOOOOOOOOOOOOOOOOOOOOO"),
+                },
+                Err(e)=> match e {
+                    crossbeam_channel::TryRecvError::Empty => (),
+                    crossbeam_channel::TryRecvError::Disconnected => eprintln!(
+                        "[ {} ]: GUICommands receiver channel disconnected: {}",
+                        "Simulation Controller".red(),
+                        e
+                    ),
+                }
             }
+        } else {
+            match self.receiver.try_recv() {
+                Ok(event) => self.handle_events(event),
+                Err(e)=> match e {
+                    crossbeam_channel::TryRecvError::Empty => (),
+                    crossbeam_channel::TryRecvError::Disconnected => eprintln!(
+                        "[ {} ]: GUICommands receiver channel disconnected: {}",
+                        "Simulation Controller".red(),
+                        e
+                    ),
+                }
+            }
+
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.heading("Drone Simulation GUI");
+
+                if ui.button("Spawn Drone").clicked() {
+                    //self.command_send.send(DroneCommand::Spawn(1)).unwrap();
+                }
+                if ui.button("Crash Drone").clicked() {
+                    //self.command_send.send(DroneCommand::Crash(1)).unwrap();
+                }
+
+                ui.separator();
+                ui.label("Simulation Events:");
+                /*match self.event_recv.try_recv() {
+                    Ok(event) => ui.label(format!("Event: {:?}", event)),
+                    Err(TryRecvError::Empty) => ui.label("No new events."),
+                    Err(TryRecvError::Disconnected) => ui.label("Error: Event channel disconnected."),
+                }*/
+            });
+
+            ctx.request_repaint(); // Refresh GUI
         }
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Drone Simulation GUI");
-
-            if ui.button("Spawn Drone").clicked() {
-                //self.command_send.send(DroneCommand::Spawn(1)).unwrap();
-            }
-            if ui.button("Crash Drone").clicked() {
-                //self.command_send.send(DroneCommand::Crash(1)).unwrap();
-            }
-
-            ui.separator();
-            ui.label("Simulation Events:");
-            /*match self.event_recv.try_recv() {
-                Ok(event) => ui.label(format!("Event: {:?}", event)),
-                Err(TryRecvError::Empty) => ui.label("No new events."),
-                Err(TryRecvError::Disconnected) => ui.label("Error: Event channel disconnected."),
-            }*/
-        });
-
-        ctx.request_repaint(); // Refresh GUI
     }
 }
