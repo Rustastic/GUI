896ba983f9fada9f5aa151f174f2e509c095b6a6
ilBuso <alessandro.busola@gmail.com>
2025-01-31 20:26:42 +0100

    gui

--- src/gui.rs
+++ src/gui.rs
@@ -17,6 +17,7 @@ pub struct SimCtrlGUI {
     edges: HashMap<NodeId, Vec<NodeId>>
 }
 
+#[derive(Clone)]
 struct DroneGUI {
     id: NodeId,
     neighbor: Vec<NodeId>,
@@ -117,16 +118,15 @@ impl SimCtrlGUI {
         Ok(())
     }
 }
-
 impl eframe::App for SimCtrlGUI {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         if !self.initialized {
             match self.receiver.try_recv() {
                 Ok(event) => match event.clone() {
                     GUIEvents::Topology(_) => self.handle_events(event),
-                    _ => panic!("CAZZZOOOOOOOOOOOOOOOOOOOOOOOOOOOO"),
+                    _ => panic!("Unexpected event received"),
                 },
-                Err(e)=> match e {
+                Err(e) => match e {
                     crossbeam_channel::TryRecvError::Empty => (),
                     crossbeam_channel::TryRecvError::Disconnected => eprintln!(
                         "[ {} ]: GUICommands receiver channel disconnected: {}",
@@ -138,7 +138,7 @@ impl eframe::App for SimCtrlGUI {
         } else {
             match self.receiver.try_recv() {
                 Ok(event) => self.handle_events(event),
-                Err(e)=> match e {
+                Err(e) => match e {
                     crossbeam_channel::TryRecvError::Empty => (),
                     crossbeam_channel::TryRecvError::Disconnected => {
                         eprintln!(
@@ -149,50 +149,54 @@ impl eframe::App for SimCtrlGUI {
                         return;
                     }
                 }
-                
             }
+
+            let mut nodes = self.nodes.clone(); // Clone `nodes` once, outside the loop to avoid borrowing conflicts
+            let edges = self.edges.clone(); // Clone `edges` once
 
             egui::CentralPanel::default().show(ctx, |ui| {
                 ui.heading("Simulation Controller");
-    
+
                 // Allocating space for drawing and preparing the painter for rendering
                 let (_response, painter) =
                     ui.allocate_painter(egui::Vec2::new(900.0, 900.0), egui::Sense::hover());
-    
+
                 // Drawing edges (connections) between drones
-                for (start_id, neighbor) in self.edges.clone() {
-                    let start = self.nodes.get(&start_id).unwrap();
-                    for end_id in neighbor {
-                        let end = self.nodes.get(&end_id).unwrap();
-                        painter.line_segment(
-                            [egui::pos2(start.x, start.y), egui::pos2(end.x, end.y)],
-                            egui::Stroke::new(2.0, Color32::LIGHT_GRAY),
-                        );
+                for (start_id, neighbor) in edges {
+                    if let Some(start) = nodes.get(&start_id) {
+                        for end_id in neighbor {
+                            if let Some(end) = nodes.get(&end_id) {
+                                painter.line_segment(
+                                    [egui::pos2(start.x, start.y), egui::pos2(end.x, end.y)],
+                                    egui::Stroke::new(2.0, Color32::LIGHT_GRAY),
+                                );
+                            }
+                        }
                     }
                 }
-    
+
                 // Drawing the nodes (drones) and handling user interaction for selection
-                for (_, pos) in self.nodes.iter_mut() {
+                for (_, pos) in nodes.iter_mut() {
                     let screen_pos = egui::pos2(pos.x, pos.y);
                     let radius = 10.0;
-    
+
                     // Allocating space for each drone's graphical representation
                     let response = ui.allocate_rect(
                         egui::Rect::from_center_size(screen_pos, egui::Vec2::splat(radius * 2.0)),
                         egui::Sense::click(),
                     );
-    
+
                     // Detecting if the drone is clicked and updating its selected status
                     if response.clicked() {
                         pos.selected = true;
                     }
-    
+
                     // Drawing the drone as a filled circle
                     painter.circle_filled(screen_pos, radius, pos.color);
                 }
-    
+
                 // Displaying a pop-up with detailed information when a drone is selected
-                for (_, instance) in self.nodes.iter_mut() {
+                for (_, instance) in nodes.iter_mut() {
                     if instance.selected {
                         egui::Window::new(format!("Node {}", instance.id))
                             .fixed_size([100.0, 100.0]) // Window size
@@ -209,7 +213,7 @@ impl eframe::App for SimCtrlGUI {
                                     ui.label(format!("PDR: {}", instance.pdr));
                                 }
                                 ui.add_space(10.0);
-    
+
                                 // Buttons to change the color of the selected drone
                                 if !instance.crashed {
                                     ui.horizontal_centered(|ui| {
@@ -240,7 +244,7 @@ impl eframe::App for SimCtrlGUI {
 
                                                     instance.crashed = true;
                                                 },
-                                                Err(e) => panic!("Voglio la mamma: {}", e),
+                                                Err(e) => panic!("Error sending command: {}", e),
                                             }
                                         }
                                         if ui.button("RemoveSender").clicked() {
@@ -260,7 +264,7 @@ impl eframe::App for SimCtrlGUI {
                                         .selected_text(instance.remove_sender_value.clone().unwrap_or("None".to_string()))
                                         .show_ui(ui, |ui| {
                                             let options: Vec<String> = instance.neighbor.iter().map(|n| n.to_string()).collect();
-                                
+
                                             for option in options {
                                                 if ui.selectable_label(
                                                     instance.remove_sender_value.as_deref() == Some(&option),
@@ -268,7 +272,7 @@ impl eframe::App for SimCtrlGUI {
                                                 ).clicked() {
                                                     // Save the selected value
                                                     instance.remove_sender_value = Some(option.clone());
-                                
+
                                                     // Parse the selected string into a NodeId
                                                     if let Ok(neighbor) = option.parse::<u8>() {
                                                         // Handle removal operation
@@ -278,7 +282,7 @@ impl eframe::App for SimCtrlGUI {
                                                     } else {
                                                         eprintln!("Failed to parse option: {}", option);
                                                     }
-                                
+
                                                     // Close the dropdown
                                                     instance.remove_sender = false;
                                                 }
@@ -287,7 +291,7 @@ impl eframe::App for SimCtrlGUI {
                                 }
 
                                 ui.add_space(10.0);
-    
+
                                 // Button to close the window
                                 if ui.button("Close").clicked() {
                                     instance.selected = false;
