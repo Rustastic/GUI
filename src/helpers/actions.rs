1b7bf33f886c641101a7ff7b0102c6c50260443e
ilBuso <alessandro.busola@gmail.com>
2025-02-04 12:48:22 +0100

    prova

--- src/helpers/actions.rs
+++ src/helpers/actions.rs
@@ -29,7 +29,7 @@ fn fruchterman_reingold(
         );
     }
 
-    let k = 90.0; // Repulsion strength (adjust as needed)
+    let k = 100.0; // Repulsion strength (adjust as needed)
     let attraction_multiplier = 0.05; // Attraction strength (adjust as needed)
     let mut temperature = 5.0; // Start with a high temperature
 
