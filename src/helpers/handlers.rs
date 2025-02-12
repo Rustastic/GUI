eabd18b688a6c62525336e88433223ee1b7fb474
ilBuso <alessandro.busola@gmail.com>
2025-02-12 16:57:26 +0100

    msg

--- src/helpers/handlers.rs
+++ src/helpers/handlers.rs
@@ -12,19 +12,12 @@ impl SimCtrlGUI {
     pub fn handle_events(&mut self, event: GUIEvents, ctx: &Context) {
         match event {
             // light up edge for 0.5 sec in green
-            GUIEvents::PacketSent(_, _, _) => {
-                /*info!("[ {} ]: Received PacketSent", "GUI".green());
-                if self.edges.get(&src).unwrap().0.contains(&dest) {
-                    self.edges.get_mut(&src).unwrap().1 = Color32::GREEN;
-                    ctx.request_repaint();
-                    thread::sleep(std::time::Duration::from_secs_f32(0.1));
-                    self.edges.get_mut(&src).unwrap().1 = Color32::GRAY;
-                } else if self.edges.get(&dest).unwrap().0.contains(&src) {
-                    self.edges.get_mut(&dest).unwrap().1 = Color32::GREEN;
-                    ctx.request_repaint();
-                    thread::sleep(std::time::Duration::from_secs_f32(0.1));
-                    self.edges.get_mut(&dest).unwrap().1 = Color32::GRAY;*/
-                }
+            GUIEvents::PacketSent(src, _, _) => {
+                info!("[ {} ]: Received PacketSent", "GUI".yellow());
+                self.nodes.get_mut(&src).unwrap().color = Color32::PURPLE;
+                ctx.request_repaint();
+                thread::sleep(std::time::Duration::from_secs_f32(0.25));
+                self.nodes.get_mut(&src).unwrap().color = Color32::LIGHT_BLUE;
             }
             // light up node  for 0.5 sec in red
             GUIEvents::PacketDropped(src, _) => {
